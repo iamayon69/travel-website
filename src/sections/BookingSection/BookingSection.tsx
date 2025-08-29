a42931309b32baf6149bffe3debe6ad455d5bd1c
Abdur Rahman Ayon <abdurrahmanayon66@gmail.com>
2025-08-29 12:21:58 +0600

    Updated the booking section

--- src/sections/BookingSection/BookingSection.tsx
+++ src/sections/BookingSection/BookingSection.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import bookingCofig from "../../utils/bookingCofig";
 
 const BookingSection = () => {
   return (
@@ -14,7 +15,23 @@ const BookingSection = () => {
           In 3 Easy Steps
         </h2>
       </div>
-      <div></div>
+      <div className="flex flex-col gap-y-8">
+        {bookingCofig.map((step, idx) => (
+          <div key={idx} className="flex items-start gap-x-4">
+            <div className="w-12 h-12 rounded-lg flex items-center justify-center bg-[#F3F4F6]">
+              <img src={step.image} alt={step.title} className="w-6 h-6" />
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold text-text-primary mb-2 font-poppins">
+                {step.title}
+              </h3>
+              <p className="text-base text-[#6B6B7A] max-w-[520px]">
+                {step.description}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
